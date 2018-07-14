ad07e9ece011d99aadce5c3ad4239a398154f1ad
Dominic Shillingford <dominic.shillingford@alumni.ubc.ca>
2018-07-14 15:36:09 -0600

    Refined the calculations for PID gain

--- software/src/functions/TapeFollow.h
+++ software/src/functions/TapeFollow.h
@@ -2,10 +2,14 @@
 
 #include <Arduino.h>
 
-// These values must be between -64 & 64.
-#define KP_TAPE 55
-#define KD_TAPE 32
+// The sum of these values must be less than 128
+#define KP_TAPE 79
+#define KD_TAPE 47
 #define KI_TAPE 1
+
+#if( KP_TAPE + KD_TAPE + KI_TAPE > 128 )
+    #error "PID gains are too high, data structures may overflow!"
+#endif
 
 #define INTEGRAL_CUTOFF 150  // This number should be less that 255 (the maximum allowed error value)
 
@@ -25,14 +29,16 @@ namespace TapeFollow {
         int16_t rawError = ( farLeft + closeLeft - closeRight - farRight ) >> 3;  // A number between -255 & 255
 
         int16_t errorProportional = KP_TAPE * rawError;
-        int16_t errorDifferencial = KD_TAPE * ( ( rawError >> 1 ) - ( prevErr >> 1 ) );
+        int16_t errorDifferential = KD_TAPE * ( ( rawError >> 1 ) - ( prevErr >> 1 ) );
         int16_t errorIntegral     = KI_TAPE * ( ( errorSum + rawError ) >> 8 );
 
-        int8_t motorCorrection = ( errorProportional + errorDifferencial ) >> 8;
+        int8_t motorCorrection;
 
         if( abs( rawError ) < INTEGRAL_CUTOFF ) {
             errorSum += rawError;
-            motorCorrection += errorIntegral >> 8;
+            motorCorrection = ( errorProportional + errorDifferential + errorIntegral ) >> 8;
+        } else {
+            motorCorrection = ( errorProportional + errorDifferential ) >> 8;
         }
 
         if( motorCorrection <= 0 ) {
