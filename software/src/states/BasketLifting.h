d8d0e13d0f4153a4b4fc3cdee2466e93c31d5c28
dgblack <davidgblack1@gmail.com>
2018-07-16 15:10:09 -0700

    Update BasketLifting.h

--- software/src/states/BasketLifting.h
+++ software/src/states/BasketLifting.h
@@ -7,23 +7,22 @@ class S_BasketLifting: public State {
     public:
         void onStart() {
             Stepper stepper = Stepper(STEPS_PER_REV,12,13);
-            liftBasket();
+            stepper.setSpeed(1200);
+            liftBasket(stepper);
             // Do something like drive forward until a switch is triggered
-            dropBasket();
+            dropBasket(stepper);
         }
 
         void onLoop() {
             
         }
         
-        void liftBasket() {
-            stepper.setSpeed(1200);
+        void liftBasket(Stepper stepper) {
             for (int i = 0; i < 13; i++)
               stepper.step(10025);
         }
         
-        void dropBasket() {
-            stepper.setSpeed(1200);
+        void dropBasket(Stepper stepper) {
             for (int i = 0; i < 13; i++)
               stepper.step(-10025);
         }
