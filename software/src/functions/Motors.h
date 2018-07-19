d93ec13d77a24fa33f99941a19ef244f4f3c7a78
Dominic Shillingford <dshillingford77@gmail.com>
2018-07-19 10:57:46 -0700

    Fixed a bug

--- software/src/functions/Motors.h
+++ software/src/functions/Motors.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <Arduino.h>
 #include "../Pins.h"
 
@@ -27,34 +26,4 @@ namespace Motors {
         digitalWrite( L_MOTOR_B, LOW );
         digitalWrite( R_MOTOR_B, LOW );        
     }
-=======
-#include <Arduino.h>
-#include "../Pins.h"
-
-namespace Motors {
-    void run( int rs, int ls ) {
-        if ( ls > 0 ) {
-            analogWrite( L_MOTOR_F, ls );
-            digitalWrite( L_MOTOR_B, 0 );
-        } else {
-            analogWrite( L_MOTOR_B, -ls );
-            digitalWrite( L_MOTOR_F, 0 );
-        }
-        
-        if ( rs > 0 ){
-            analogWrite( R_MOTOR_F, rs );
-            digitalWrite( R_MOTOR_B, 0 );
-        } else {
-            analogWrite( R_MOTOR_B, -rs );
-            digitalWrite( R_MOTOR_F, 0 );
-        }
-    }
-
-    void stop() {
-        digitalWrite( L_MOTOR_F, LOW );
-        digitalWrite( R_MOTOR_F, LOW );
-        digitalWrite( L_MOTOR_B, LOW );
-        digitalWrite( R_MOTOR_B, LOW );        
-    }
->>>>>>> a3a9912d9915c94c9e40ae0e55b48e57926a8bdc
-}
+};
