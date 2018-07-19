d575d9592477747604ed6ed3d090f446b51c19b5
Dominic <dshillingford77@gmail.com>
2018-07-19 12:07:42 -0700

    Added a new task to create a new state

---
+++ software/tasks/initstate.sh
@@ -0,0 +1,6 @@
+if [ $# -ne 1 ]
+then 
+    echo "Please provide a single argument, the name of the state to be constructed."
+else
+    sed "s/{1}/$1/" ../misc/SampleState.h > ../src/states/$1.h
+fi
