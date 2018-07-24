e4694fd590916252d2ac54ed6a3617f28ee3e33f
Dominic Shillingford <dshillingford77@gmail.com>
2018-07-24 09:29:47 -0700

    Some restructuring

--- software/initstate.sh
+++ software/initstate.sh
@@ -2,5 +2,5 @@ if [ $# -ne 1 ]
 then 
     echo "Please provide a single argument, the name of the state to be constructed."
 else
-    sed "s/{1}/$1/" ../misc/SampleState.h > ../src/states/$1.h
+    sed "s/{1}/$1/" misc/SampleState.h > src/states/$1.h
 fi
