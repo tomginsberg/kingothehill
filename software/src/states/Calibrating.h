89aec37e4241c78d6f5dcb660dea4826d1c1a7af
Dominic Shillingford <d.shillingford@alumni.ubc.ca>
2018-07-31 19:41:42 -0700

    Added helper function to read mapped tf values

--- software/src/states/Calibrating.h
+++ software/src/states/Calibrating.h
@@ -4,6 +4,7 @@
 #include "../Pins.h"
 #include "../GlobalVariables.h"
 #include "../SerialIDs.h"
+#include "../functions/TapeFollow.h"
 
 class S_Calibrating: public State {
     void onStart() { 
@@ -20,14 +21,14 @@ class S_Calibrating: public State {
         Serial.write( INIT_PLATFORMS );
         Serial.write( DETACH_PLATFORMS );
 
-        LEFT_EDGE_BASELINE = analogRead( TF_EDGE_LEFT );
+        LEFT_EDGE_BASELINE  = analogRead( TF_EDGE_LEFT  );
         RIGHT_EDGE_BASELINE = analogRead( TF_EDGE_RIGHT );
-        SETPOINT = map( analogRead( TF_FAR_LEFT ), 80, 750, 60, 600 ) + analogRead( TF_CLOSE_LEFT ) - 
-                    analogRead( TF_CLOSE_RIGHT ) - map( analogRead( TF_FAR_RIGHT ), 120, 400, 60, 600 );
+
+        SETPOINT = ( TapeFollower::readFarLeft()  + TapeFollower::readCloseLeft()  ) -
+                   ( TapeFollower::readFarRight() + TapeFollower::readCloseRight() );  
     }
 
     void onEnd() {
-        
         Serial.end();
     }
 
