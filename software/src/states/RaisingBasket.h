9e225aaee3a25e5b602f0ad5835cb2bd0825cc69
Dominic Shillingford <d.shillingford@alumni.ubc.ca>
2018-08-02 16:32:48 -0700

    Fixed a typo

--- software/src/states/RaisingBasket.h
+++ software/src/states/RaisingBasket.h
@@ -9,15 +9,9 @@ class S_RaisingBasket: public State {
         Motors::run( -100 );
         Serial.begin( 9600 );
         Serial.write( RAISE_BASKET );
-<<<<<<< HEAD
         delay( 400 );
         Motors::stop();
         delay( 11500 );
-=======
-        delay( 700 );
-        Motors::stop();
-        delay( 9500 );
->>>>>>> 7bc926b5364ccb42efce1d444fd1293909c06cf0
     }
 
     bool transitionCondition() {
