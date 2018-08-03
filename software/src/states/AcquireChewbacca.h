24cbb36b8dc6a32998141a0fe6f1027f5a05dfcd
tomginsberg <tommyginsberg@gmail.com>
2018-08-03 10:37:57 -0700

    Aquire chewbacca better

--- software/src/states/AcquireChewbacca.h
+++ software/src/states/AcquireChewbacca.h
@@ -12,13 +12,15 @@ class S_AcquireChewbacca: public State {
         delay( 1200 );
         Serial.write( LIFT_L_CLAW );
         delay( 1500);
-        Serial.write( OPEN_L_CLAW );
+        Serial.write( LEFT_CLAW_OPEN_WIDE );
         delay( 1000 );
     }
 
     void onEnd() {
         Serial.write( LOWER_L_CLAW );
-        delay( 700 );
+        delay( 200 );
+        Serial.write ( OPEN_L_CLAW );
+        delay( 500 );
         Serial.write( DETACH_L_CLAW );
         Serial.flush();
         Serial.end();
