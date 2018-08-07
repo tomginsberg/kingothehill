1fbd98b09a2e0128473932b7a8d260c13d67ba4a
Dominic Shillingford <d.shillingford@alumni.ubc.ca>
2018-08-07 14:31:44 -0700

    Changed edge finding algorithm

--- software/src/states/FindingBridgeEdge.h
+++ software/src/states/FindingBridgeEdge.h
@@ -6,10 +6,24 @@
 #include "../GlobalVariables.h"
 
 class S_FindingBridgeEdge: public State {
+    uint16_t state = 10;
+
     void onStart() { 
         Motors::run( 110 );
         delay( 400 );
         Motors::run( -10, 130 );
+    }
+
+    void onLoop() {
+        switch( state ) {
+            case 10: 
+            {
+                if( analogRead( TF_EDGE_LEFT ) > LEFT_EDGE_BASELINE + LEFT_EDGE_THR ) {
+                    state = 20;
+                    Motors::run( -10, 130 );
+                }
+            }
+        }
     }
 
     void onEnd() { 
@@ -19,6 +33,6 @@ class S_FindingBridgeEdge: public State {
 
     bool transitionCondition() {
         // <tt>ClimbingBridge<tt> 
-        return analogRead( TF_EDGE_RIGHT ) > LEFT_EDGE_BASELINE + 200;
+        return state == 20 && analogRead( TF_EDGE_RIGHT ) > LEFT_EDGE_BASELINE + 200;
     }
 };
